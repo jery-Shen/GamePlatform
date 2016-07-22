09dc0e93fb9b46ea2ffdfe3eca9b482d9ec1aa9f
shenyaojie <syj@mjcm.com>
2016-07-22 11:02:11 +0800

    new

---
+++ app/scripts/controllers.js
@@ -0,0 +1,45 @@
+angular.module('starter.controllers', [])
+
+.controller('RecommendCtrl', function($scope) {
+
+  $scope.sign = function(){
+    console.log('签到');
+    location.href='#/about';
+  };
+  $scope.task = function(){
+    console.log('附近的人');
+    location.href='#/about';
+  };
+  $scope.newGames=[
+    {'name':'卡通足球宴','image':'images/y1.png','url':''},
+    {'name':'带球冲刺','image':'images/y2.png','url':''},
+    
+  ];
+  $scope.hotGames=[
+    {'name':'开心找弹珠','image':'images/y3.png','url':''},
+    {'name':'六角碎片','image':'images/y4.png','url':''},
+    {'name':'大白回家','image':'images/y5.png','url':''},
+    {'name':'疯狂撑杆跳','image':'images/y6.png','url':''},
+    {'name':'足球明星2','image':'images/y7.png','url':''},
+    {'name':'一起摇摆','image':'images/y8.png','url':''},
+    {'name':'植物大战僵尸','image':'images/y9.png','url':''},
+    {'name':'沿着线走','image':'images/y10.png','url':''},
+    {'name':'一笔练成','image':'images/y11.png','url':''},
+    {'name':'电动切割狂','image':'images/y12.png','url':''},
+    {'name':'插插插','image':'images/y13.png','url':''},
+    {'name':'小白人快跑','image':'images/y14.png','url':''},
+  ];
+  
+})
+
+.controller('DiscoverCtrl', function($scope) {
+ 
+})
+
+
+
+.controller('AccountCtrl', function($scope) {
+  $scope.setting = function(){
+    console.log('setting')
+  };
+});
